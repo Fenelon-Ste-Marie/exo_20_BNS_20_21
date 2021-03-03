8de3147b886d74194ba5914865efe8c6a84b91c0
ValMssx <67736939+ValMssx@users.noreply.github.com>
2021-03-03 11:19:28 +0100

    Update exercice2.py

--- exercices/exercice2.py
+++ exercices/exercice2.py
@@ -1,10 +1,13 @@
-Pieces = [100,50,20,10,5,2,1]
-def rendu_glouton(arendre, solution=[], i=0):
-    if arendre == 0:
-        return ...
-    p = Pieces[i] #correction
-    if p <= ... :
-        solution.append(...) #correction
-        return rendu_glouton(arendre - p,solution ,i) #correction
-    else :
-        return rendu_glouton(arendre, solution, ...)
+def inverse_chaine(chaine):
+    result = ...
+    for caractere in chaine:
+       result = ...
+    return result
+
+def est_palindrome(chaine):
+    inverse = inverse_chaine(chaine)
+    return ...
+
+def est_nbre_palindrome(nbre):
+    chaine = ...
+    return est_palindrome(chaine)
