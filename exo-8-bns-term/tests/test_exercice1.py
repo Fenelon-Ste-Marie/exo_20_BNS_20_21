e5f9bc4060ab9b085a83b7c72bf5cf0654063db9
ValMssx <67736939+ValMssx@users.noreply.github.com>
2021-03-03 11:15:17 +0100

    Delete exo-8-bns-term directory

--- exo-8-bns-term/tests/test_exercice1.py
+++
@@ -1,6 +0,0 @@
-from exercices.exercice1 import *
-
-def test_recherche():
-    assert recherche('e', "sciences") == 2
-    assert recherche('i',"mississippi") == 4
-    assert recherche('a',"mississippi") == 0
