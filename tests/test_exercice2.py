ab0b35e6f399e63240e1e24bfcfe7e3cbd56411c
ValMssx <67736939+ValMssx@users.noreply.github.com>
2021-03-03 11:29:13 +0100

    Update test_exercice2.py

--- tests/test_exercice2.py
+++ tests/test_exercice2.py
@@ -1,5 +1,13 @@
 from exercices.exercice2 import *
 
-def test_rendu_glouton():
-    assert rendu_glouton(68,[],0) == [50, 10, 5, 2, 1]
-    assert rendu_glouton(291,[],0) == [100, 100, 50, 20, 20, 1]
+def test_inversion():
+    chaine='bac'
+    assert inverse_chaine(chaine) == 'cab'
+    
+def test_palindrome():
+    chaine='NSI'
+    assert est_palindrome(chaine) ==False
+
+def test_nbre_palindrome():
+    assert est_nbre_palindrome(214312) == False
+    assert est_nbre_palindrome(213312) == True
