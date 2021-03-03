1e930be1a49d2a8b03efafd379141da3adef1443
ValMssx <67736939+ValMssx@users.noreply.github.com>
2021-03-03 11:11:33 +0100

    Initial commit

---
+++ tests/test_exercice2.py
@@ -0,0 +1,5 @@
+from exercices.exercice2 import *
+
+def test_rendu_glouton():
+    assert rendu_glouton(68,[],0) == [50, 10, 5, 2, 1]
+    assert rendu_glouton(291,[],0) == [100, 100, 50, 20, 20, 1]
